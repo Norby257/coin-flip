e80e3713cb296c90a134390f129c7eb6df14f3b5
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-09 13:59:24 -0600

    added logic

--- game.js
+++ game.js
@@ -5,6 +5,29 @@ console.log("We are linked");
 //wins
 //losses 
 var userGuess;
-var computerGuess;
-var randomNumber;
+var wins;
+var losses;
+var computerChoices = ["h", "t"];
+//computer picks random number 
+////add function -user types a key 
+document.onkeyup = function(event) {
+    //store the result of that 
+   var userGuess = event.key;
+   //test to see if this works:
+   console.log(userGuess);
+   alert("You pressed " + userGuess);
+   //make computer pick a key 
+   var computerChoice = computerChoices[Math.floor(Math.random * computerChoices.length)];
+   if(computerChoice === userGuess) {
+       alert("You win!");
+       wins++;
+   }
 
+   
+   //compare results  - if match, then wins++; else losses++
+
+
+};
+//make computer pick a key 
+//how to assign computer choice 
+//how to select jquery 
